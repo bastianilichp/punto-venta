c8458727949cd7f1dbe30054571f7807b53ffea4
Bastian Venegas <bvenegas@actis.cl>
2025-04-23 15:18:15 -0400

    modificacion jar

---
+++ src/main/java/cl/puntoventa/app/main/PuntoVenta.java
@@ -0,0 +1,14 @@
+package cl.puntoventa.app.main;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class PuntoVenta {
+    public static void main(String[] args) {
+        SpringApplication.run(PuntoVenta.class, args);
+    }
+}
