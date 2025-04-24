8d2d52f329382f47fece690297175ff5b337fe6e
Bastian Venegas <bvenegas@actis.cl>
2025-04-24 10:20:26 -0400

    Update setup-payara.sh

--- bin/setup-payara.sh
+++ bin/setup-payara.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Descargando Payara Micro..."
 mkdir -p server
-curl -L https://search.maven.org/remotecontent?filepath=fish/payara/distributions/payara-micro/6.2024.2/payara-micro-6.2024.2.jar -o server/payara-micro-latest.jar
+curl -L https://repo1.maven.org/maven2/fish/payara/distributions/payara-web/6.2024.12/payara-web-6.2024.12-sources.jar -o server/payara-micro-latest.jar
 echo "Payara Micro descargado correctamente."
